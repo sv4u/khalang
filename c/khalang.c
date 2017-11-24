5d2baa71455f54e974ae999ffe877411eca889f6
Sasank Vishnubhatla <sasank@vishnubhatlas.net>
2017-11-23 22:22:46 -0500

    Added parser and forward defs

--- c/khalang.c
+++ c/khalang.c
@@ -15,6 +15,54 @@ char *readline (char *prompt) {
 
 void add_history(char *empty) {}
 
+// Parser Definitions
+
+mpc_parser_t *Number;
+mpc_parser_t *Symbol;
+mpc_parser_t *String;
+mpc_parser_t *Comment;
+mpc_parser_t *Sexpr;
+mpc_parser_t *Qexpr;
+mpc_parser_t *Expr;
+mpc_parser_t *Khaled;
+
+// Forward Definitions
+
+struct lval;
+struct lenv;
+typedef struct lval lval;
+typedef struct lenv lenv;
+
+// Khaled Values
+
+enum {LVAL_ERR, LVAL_NUM, LVAL_SYM, LVAL_STR, LVAL_FUN, LVAL_SEXPR, LVAL_QEXPR};
+
+typedef lval*(*lbuiltin) (lenv*, lval*);
+
+struct lval {
+	int type;
+
+	long num;
+	char *err;
+	char *sym;
+	char *str;
+
+	lbuiltin builtin;
+	lenv* env;
+	lval* formals;
+	lval* body;
+
+	int count;
+	lval **cells;
+};
+
+lval *lval_num (long x) {
+	lval *v = malloc(sizeof(lval));
+	v->type = LVAL_NUM;
+	v->num = x;
+	return v;
+}
+
 int main(int argc, char **argv) {
 	return 0;
 }
